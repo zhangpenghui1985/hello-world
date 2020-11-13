20df8a21d907c600ade58bb76f5c0780bd360c56
zhangpenghui1985 <zhangpenghui1985@aliyun.com>
2020-11-13 11:29:00 +0800

    Update ServerStarter.java
    修改注释

--- src/main/java/ServerStarter.java
+++ src/main/java/ServerStarter.java
@@ -5,19 +5,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
 
-/********************************************
- * 文件名称: ServerStarter.java
- * 系统名称: 理财登记过户平台
- * 模块名称:
- * 软件版权: 恒生电子股份有限公司
- * 功能说明:
- * 系统版本: 6.0
- * 开发人员: 葛跃云
- * 开发时间: 2020/3/25
- * 审核人员: 
- * 相关文档:
- * 修改记录: 修改日期    修改人员    修改说明
- *********************************************/
 @EnableCaching
 @SpringBootApplication(scanBasePackages={"com.hundsun.jres","com.hundsun.lcpt","com.zhangph.test"},
         exclude= {DataSourceAutoConfiguration.class})
